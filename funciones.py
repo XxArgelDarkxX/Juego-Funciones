e87ccb34bf48a3918d2f25773bb964b0922132f3
sebastian rubio <rubio.sebastian@correounivalle.edu.co>
2024-05-30 15:52:40 -0500

    oee

--- funciones.py
+++ funciones.py
@@ -1,9 +1,20 @@
 import random 
 import math
-import customtkinter as ctk
+import modificar_interfaz as mi
+
+def respuestas(respond):
+    return respond
+
+def preguntas(pregunta):
+    return pregunta
+    
 
 
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 def random_f(): #funcion de py para elegir una funcion aleatoria
     funciones = ["  funcion lineal", "funcion cuadratica", "funcion cubica", "funcion raiz cuadrada", "funcion parte entera", "funcion valor absoluto", "funcion inversa"]
     funcion_aleatoria = "funcion raiz cuadrada"
@@ -156,41 +167,32 @@ def f_raiz_cuadrada():
     global suma4
     suma4 =1
     functions = [
-    ("sqrt(x)", "R", "0", "0"),                                 # Función raíz cuadrada de x
-    ("sqrt(x^2 - 9)", "R", "-3, 3", "0"),                       # Función raíz cuadrada de x^2 - 9
-    ("sqrt(4x^2 + 16x + 16)", "R", "-2", "0","4"),                 # Función raíz cuadrada de 4x^2 + 16x + 16
-    ("sqrt(x-1)", "1,00", "1,0", "0"),            # Función raíz cúbica de x-1
-    ("sqrt(4-x^2)", "-2,2","0,2")          # Función raíz cuadrativa de 4-x^2
+    ["sqrt(x)", "R", "0", "0"],                              # Función raíz cuadrada de x
+    ["sqrt(x^2 - 9)", "R", "-3, 3", "0"],                       # Función raíz cuadrada de x^2 - 9
+    ["sqrt(4x^2 + 16x + 16)", "R", "-2", "0","4"] ,               # Función raíz cuadrada de 4x^2 + 16x + 16
+    ["sqrt(x-1)", "1,00", "1,0", "0"],            # Función raíz cúbica de x-1
+    ["sqrt(4-x^2)", "-2,2","0,2"]         # Función raíz cuadrativa de 4-x^2
 ]
+    
     if suma4 == 1:
-        print("¡Rapido analiza la siguiente funcion: ", functions[0][0])
+        pregunta=f"¡Rapido analiza la siguiente funcion: {functions[0][0]} "
         pregunta1= "Dominio: "
         pregunta2= "Eje X: "
         pregunta3= "Eje Y: "
-        respuestas(functions[0])
-        dominio=input("Dominio: ")
-        preguntas(pregunta1)
-        eje_x=input("Eje X: ")
-        preguntas(pregunta2)
-        eje_y=input("Eje Y: ")
-        preguntas(pregunta3)
-        if dominio == functions[0][1] and eje_x == functions[0][2] and eje_y == functions[0][3]:
-            print("¡Correcto!")
-        else:
-            print("¡Incorrecto!")
-            print("El dominio de la función raíz cuadrada de x es R, el eje X es 0 y el eje Y es 0")
+        #print("El dominio de la función raíz cuadrada de x es R, el eje X es 0 y el eje Y es 0")
+        lista_preguntas=[pregunta,pregunta1,pregunta2,pregunta3]
+        lista_respuestas=[functions[0][1],functions[0][2],functions[0][3]]
+        return lista_preguntas,lista_respuestas
     elif suma4==2:
-        print("¡Rapido analiza la siguiente funcion: ", functions[1][0])
-        respuestas(functions[1])
-        dominio=input("Dominio: ")
-        eje_x=input("Eje X 1: ")
-        eje_x2=input("Eje X 2: ")
-        eje_y=input("Eje Y 1: ")
-        if dominio == functions[1][1] and eje_x == functions[1][2] and eje_x2 == functions[1][3] and eje_y == functions[1][4]:
-            print("¡Correcto!")
-        else:
-            print("¡Incorrecto!")
-            print("El dominio de la función raíz cuadrada de x^2 - 9 es R, el eje X es -3 y 3 y el eje Y es 0 ")
+        pregunta=f"¡Rapido analiza la siguiente funcion: {functions[1][0]} "
+        pregunta1= "Dominio: "
+        pregunta2= "Eje X 1: "
+        pregunta3= "Eje X 2: "
+        pregunta4= "Eje Y: "
+        lista_preguntas=[pregunta,pregunta1,pregunta2,pregunta3,pregunta4]
+        lista_respuestas=[functions[1][1],functions[1][2],functions[1][2],functions[1][3]]
+        return lista_preguntas,lista_respuestas
+        #print("El dominio de la función raíz cuadrada de x^2 - 9 es R, el eje X es -3 y 3 y el eje Y es 0 ")
     elif suma4==3:
         print("¡Rapido analiza la siguiente funcion: ", functions[2][0])
         respuestas(functions[2])
@@ -333,4 +335,7 @@ if __name__ == '__main__':
     # cuadratica()
     # cubica()
     random_f()
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
