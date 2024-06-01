679ec8e7e641d871c25942e00ac6a82848edd4d9
sebastian rubio <rubio.sebastian@correounivalle.edu.co>
2024-05-31 22:19:27 -0500

    papi somos los mejores

--- modificar_interfaz.py
+++ modificar_interfaz.py
@@ -5,24 +5,26 @@ from tkinter import messagebox as mb
 import time as t
 from PIL import Image, ImageTk
 import tkinter as tk
-global i,vidas
+global i,vidas,puntos
+puntos=0
 i=0
 vidas=9
 
 
 # verifica que la respuesta este bien y que la lista no haya acabado
-def verificar(respond,respond_entry,label_see,ask,label_ask,button,corazones,ventana,foto):
-    global i,vidas
+def verificar(respond,respond_entry,label_see,ask,label_ask,button,corazones,ventana,foto,aciertos):
+    global i,vidas,puntos
     if respond==respond_entry.get(): # <--- Aquí se compara la respuesta del usuario con la respuesta correcta
         respond_entry.delete(0, ctk.END)
         mb.showinfo("Correcto","Respuesta correcta") # <--- Aquí se muestra un mensaje de que la respuesta es correcta 
-        
+        puntos+=1
+        aciertos.configure(text="Aciertos: "+str(puntos)) # <--- Aquí se actualiza el número de aciertos
         
         i+=1
         if i==len(ask) : # <--- Aquí se verifica si la lista de preguntas se acabó para reiniciarla
             i=0 # <--- Aquí se reinicia el contador de preguntas
             preguntas,respuestas,pregunta=f.random_f() # <--- Aquí se obtienen nuevas preguntas
-            return modificar_interfaz(preguntas,pregunta,respuestas,label_ask,button,respond_entry,label_see,corazones,ventana,foto) # <--- Aquí se llama a la función que modifica la interfaz
+            return modificar_interfaz(preguntas,pregunta,respuestas,label_ask,button,respond_entry,label_see,corazones,ventana,foto,aciertos) # <--- Aquí se llama a la función que modifica la interfaz
         label_see.configure(text=ask[i])  # <--- Aquí se actualiza la pregunta 
         label_see.update_idletasks()  #  <-- actualiza la interfaz
         respond_entry.delete(0, ctk.END) # <--- Aquí se borra el contenido del entry
@@ -37,6 +39,7 @@ def verificar(respond,respond_entry,label_see,ask,label_ask,button,corazones,ven
             respuesta=mb.showinfo("intentae","quieres volver a intentar?",type="yesno")
             if respuesta=="yes":
                 vidas=9
+                puntos=0 
                 ventana.destroy()
                 interfaz.juego()
             else:
@@ -46,12 +49,12 @@ def verificar(respond,respond_entry,label_see,ask,label_ask,button,corazones,ven
         if i==len(ask) : # <--- Aquí se verifica si la lista de preguntas se acabó para reiniciarla
             i=0 # <--- Aquí se reinicia el contador de preguntas
             preguntas,respuestas,pregunta=f.random_f() # <--- Aquí se obtienen nuevas preguntas
-            return modificar_interfaz(preguntas,pregunta,respuestas,label_ask,button,respond_entry,label_see,corazones,ventana,foto)  # <--- Aquí se llama a la función que modifica la interfaz
+            return modificar_interfaz(preguntas,pregunta,respuestas,label_ask,button,respond_entry,label_see,corazones,ventana,foto,aciertos)  # <--- Aquí se llama a la función que modifica la interfaz
         label_see.configure(text=ask[i])  # <--- Aquí se actualiza la pregunta
         label_see.update_idletasks()  #  <-- actualiza la interfaz
         return False 
 
-def modificar_interfaz(ask,see,respond,label_ask,button,respond_entry,label_see,corazones,ventana,foto): # <--- Aquí se crea la función que modifica la interfaz
+def modificar_interfaz(ask,see,respond,label_ask,button,respond_entry,label_see,corazones,ventana,foto,aciertos): # <--- Aquí se crea la función que modifica la interfaz
     global i,vidas # <--- Aquí se declara la variable global i
     if i==len(ask): # <--- Aquí se verifica si la lista de preguntas se acabó para reiniciarla 
         i=0 
@@ -59,4 +62,4 @@ def modificar_interfaz(ask,see,respond,label_ask,button,respond_entry,label_see,
         ask,see,respond=f.random_f() # <--- Aquí se obtienen nuevas preguntas
     label_ask.configure(text=see)  # <--- Aquí se actualiza la función
     label_see.configure(text=ask[i]) # <--- Aquí se actualiza la pregunta
-    button.configure(command=lambda: verificar(respond[i],respond_entry,label_see,ask,label_ask,button,corazones,ventana,foto)) # <--- Aquí se le asigna la función verificar al botón
+    button.configure(command=lambda: verificar(respond[i],respond_entry,label_see,ask,label_ask,button,corazones,ventana,foto,aciertos)) # <--- Aquí se le asigna la función verificar al botón
